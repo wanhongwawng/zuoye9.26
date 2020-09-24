5c800a950269efb12289b560afe00e7a1b995d2e
wanhongwawng <1724993088@qq.com>
2020-09-24 23:13:49 +0800

    第3次提交

---
+++ controllers/default.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"beego02/models"
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+	"io/ioutil"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+func (c *MainController) Post() {
+	var person models.Person
+    dataBytes,err:=ioutil.ReadAll(c.Ctx.Request.Body)
+    if err!=nil{
+    	c.Ctx.WriteString("数据接收错误，请重试")
+		return
+	}
+	err=json.Unmarshal(dataBytes,&person)
+	if err!=nil {
+		c.Ctx.WriteString("数据解释失败，请重试")
+		return
+	}
+	c.Ctx.WriteString("数据解析成功")
+	fmt.Println("姓名",person.Name)
+	fmt.Println("年龄",person.Age)
+	fmt.Println("性别",person.Sex)
+}
